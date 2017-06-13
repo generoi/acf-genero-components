c36e05268700bc07acfa666becef0b13640ddb4a
Oskar Schöldström <m@oxy.fi>
2017-06-13 17:29:40 -0500

    Add ArchivePageComPonent

---
+++ src/ArchivePageComponent.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Genero\Component;
+
+use Timber;
+
+class ArchivePageComponent implements ComponentInterface
+{
+    protected static $groupKey = 'group_593ff043dd4db';
+
+    public function __construct()
+    {
+        if ($this->validateRequirements()) {
+            add_filter('acf/load_field/name=archive__post_type', [$this, 'post_type_choices'], 9);
+            add_filter('page_template_hierarchy', [$this, 'add_template_suggestions'], 9);
+            add_action('post_type_archive_link', [$this, 'post_type_archive_link'], 10, 2);
+            add_filter('timber/context', [$this, 'add_timber_context'], 9);
+            // Add options which are not overridable.
+            require_once __DIR__ . '/ArchivePageComponent/acf-archivepage-component.php';
+        }
+    }
+
+    public function post_type_archive_link($link, $post_type)
+    {
+        $pages = get_pages([
+            'meta_key' => 'archive__post_type',
+            'meta_value' => $post_type,
+            'number' => 1,
+        ]);
+        if (!empty($pages)) {
+            $page = reset($pages);
+            return get_permalink($page);
+        }
+        return $link;
+    }
+
+    public function add_template_suggestions($templates) {
+        $post = get_post();
+        if ($post && $archive = get_field('archive__post_type', $post)) {
+            array_splice($templates, 2, 0, ["archive-${archive}.php", "archive.php"]);
+        }
+        return $templates;
+    }
+
+    public function add_timber_context($context)
+    {
+        $object = get_queried_object();
+        if (!isset($object->post_type) || $object->post_type != 'page') {
+            return $context;
+        }
+
+        if ($post_type = get_field('archive__post_type', $object->ID)) {
+            $context['post'] = new Timber\Post($object);
+            $context['posts'] = new Timber\PostQuery([
+                'post_type' => $post_type,
+                'posts_per_page' => get_field('archive__posts_per_page', $object->ID),
+                'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
+            ]);
+        }
+        return $context;
+    }
+
+    public function post_type_choices($field)
+    {
+        $post_types = get_post_types([
+            'public' => true,
+            '_builtin' => false,
+        ], 'objects');
+
+        foreach ($post_types as $post_type) {
+            $field['choices'][$post_type->name] = $post_type->label;
+        }
+        return $field;
+    }
+
+    public function validateRequirements()
+    {
+        return function_exists('get_field');
+    }
+}
---
+++ src/ArchivePageComponent/acf-archivepage-component.php
@@ -0,0 +1,73 @@
+<?php
+// @codingStandardsIgnoreFile
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_593ff043dd4db',
+	'title' => 'Archive options',
+	'fields' => array (
+		array (
+			'key' => 'field_593ff04d5ca2d',
+			'label' => 'Archive for',
+			'name' => 'archive__post_type',
+			'type' => 'select',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array (
+				'post' => 'Posts',
+			),
+			'default_value' => array (
+			),
+			'allow_null' => 1,
+			'multiple' => 0,
+			'ui' => 0,
+			'ajax' => 0,
+			'return_format' => 'value',
+			'placeholder' => '',
+		),
+		array (
+			'key' => 'field_593ff08d5ca2e',
+			'label' => 'Posts per page',
+			'name' => 'archive__posts_per_page',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => 10,
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
